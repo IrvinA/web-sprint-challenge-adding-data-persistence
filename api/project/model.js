f49f7581f480f74f2efcd590971fd47b55ff5980
IrvinA <arevalosirvin@gmail.com>
2021-12-09 15:54:36 -0800

    project model complete

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,18 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig');
+
+function getProjects() {
+  return db('projects');
+}
+
+function addProject(project) {
+  return db('projects')
+    .insert(project)
+    .then(([project_id]) => {
+      return db('projects').where('project_id', project_id).first();
+    });
+}
+
+module.exports = {
+  getProjects,
+  addProject,
+};
