932e4603d289e71ef11c20d8baf9858f538cf11e
IrvinA <arevalosirvin@gmail.com>
2021-12-09 15:33:33 -0800

    server.js complete

--- api/server.js
+++ api/server.js
@@ -1,1 +1,23 @@
-// build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmet');
+const projectsRouter = require('./project/router');
+const resourcesRouter = require('./resource/router');
+const tasksRouter = require('./task/router');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use('/api/resources', resourcesRouter);
+server.use('/api/projects', projectsRouter);
+server.use('/api/tasks', tasksRouter);
+
+// eslint-disable-next-line no-unused-vars
+server.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
