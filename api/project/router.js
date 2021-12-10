51dfd2dc5bfd00c686430d58b86839775ff17eb4
IrvinA <arevalosirvin@gmail.com>
2021-12-09 16:39:31 -0800

    project router complete

--- api/project/router.js
+++ api/project/router.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const Projects = require('./model');
-const {} = require('./middleware');
+const { checkPost, validatePost } = require('./middleware');
 
 const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  Projects.getProjects()
+    .then((projects) => {
+      res.json(projects);
+    })
+    .catch(next);
+});
+
+router.post('/', checkPost, validatePost, (req, res, next) => {
+  Projects.addProject(req.body)
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch(next);
+});
 
 module.exports = router;
